d5d1813289676742e4729e5818870fd4b1c8c2bb
Wellington Pereira de Carvalho <wellpcarv@hotmail.com>
2024-09-16 11:41:30 -0300

    Subindo Projeto de Integração Português e História

---
+++ js/btnjm.js
@@ -0,0 +1,12 @@
+const btnScrollToTop = document.getElementById("click");
+
+btnScrollToTop.addEventListener("click", function() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+
+
+})
+
